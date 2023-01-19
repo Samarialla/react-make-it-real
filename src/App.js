b96e3dd5337db5f3dba21ba6f5825eedf828dd28
Sergio Amarilla <sergojavier23@gmail.com>
2023-01-18 21:16:04 -0300

    mejroas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <h1>Mi primera aplicación</h1>
-        <p>Casa</p>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
